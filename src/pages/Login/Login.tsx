fe3ff70818fcfe33d808ade440c087ab9f04da26
user1 <user1@user1ui-MacBook-Air.local>
2021-11-03 23:00:18 +0900

    Add : 타입스크립트 설치 및 세팅

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+function Login(){
+
+  let 이름 :string = 'kim'
+
+  return(
+    <div>
+      <p>테스트용</p>
+    </div>
+    
+  )
+} 
+
+export default Login;
